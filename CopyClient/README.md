b384ace52864a87b1ab5b02dd0b60e8c41ac0937
uriya1346 <95591955+uriya1346@users.noreply.github.com>
2023-07-16 09:52:02 +0300

    #

--- CopyClient/README.md
+++
@@ -1,6 +0,0 @@
-"# Rental-Sale-client" 
-APIS : 
--google maps geocoding
--mapbox 
--paypal
--emailJS
--- CopyClient/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./comps/css/index.css";
-import App from "./App";
-
-createRoot(document.getElementById('root')).render(<App />);
